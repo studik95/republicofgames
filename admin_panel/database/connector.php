4111de5f7621107172a531bb77b9ec14adb5a0e0
Michal Stodolny <m.michalstodolny@gmail.com>
2021-03-06 14:07:09 +0100

    Added a connector.php file for database connecting.

---
+++ admin_panel/database/connector.php
@@ -0,0 +1,9 @@
+<?php
+
+$host = 'localhost';
+$port = 5432;
+$dbname = 'republicofgames';
+$user = 'michal';
+$password = 'michal';
+
+$dsn = "pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password";
