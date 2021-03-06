ae6f9eaff8ddc6599e4c384bedd22b38b5775363
Michal Stodolny <m.michalstodolny@gmail.com>
2021-03-06 15:50:38 +0100

    Added filtering data from game form by trim function and then htmspecialchars function.

--- admin_panel/easyinsertdata/backend/game.php
+++ admin_panel/easyinsertdata/backend/game.php
@@ -1,5 +1,25 @@
 <?php
 
+//information about game
+$title =  htmlspecialchars(trim($_POST['title']));
+$title_pl = htmlspecialchars(trim($_POST['title_pl']));
+$producer = htmlspecialchars(trim($_POST['producer']));
+$publisher = htmlspecialchars(trim($_POST['publisher']));
+$cover = htmlspecialchars(trim($_POST['cover']));
+$continuation = htmlspecialchars(trim($_POST['continuation']));
+$addition = htmlspecialchars(trim($_POST['addition']));
+$description = htmlspecialchars(trim($_POST['description']));
+//requirements
+$os = htmlspecialchars(trim($_POST['os']));
+$minimum_processor = htmlspecialchars(trim($_POST['minimum_processor']));
+$minimum_graphics = htmlspecialchars(trim($_POST['minimum_graphics']));
+$minimum_memory = htmlspecialchars(trim($_POST['minimum_memory']));
+$recommended_processor = htmlspecialchars(trim($_POST['recommended_processor']));
+$recommended_graphics = htmlspecialchars(trim($_POST['recommended_graphics']));
+$recommended_memory = htmlspecialchars(trim($_POST['recommended_memory']));
+$storage = htmlspecialchars(trim($_POST['storage']));
+$notes = htmlspecialchars(trim($_POST['notes']));
 
+echo $title;
 
 ?>
